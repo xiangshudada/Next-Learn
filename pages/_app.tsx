6851ed3ba102e4abdb0e4e90d0a279ef29d933f6
zlpei <812476105@qq.com>
2024-08-30 17:13:31 +0800

    add github login page

--- pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  const getLayout = Component.getLayout || ((page: any) => page);
-  return getLayout(<Component {...pageProps} />);
-}
